ce2d23c06fbbd44156e9cf9936128cf03bc3fe37
ypooja11 <47106500+ypooja11@users.noreply.github.com>
2021-11-06 17:16:29 -0400

    Add files via upload

--- src/script1a.js
+++ src/script1a.js
@@ -1,7 +1,7 @@
-d3.csv("dataaset/01-AB n_gender_year.csv").then(function(dataset){
+d3.csv("dataset/01-AB_n_gender_year.csv").then(function(dataset){
 	 var dimensions = {
-        width: 2000,
-        height: 2000,
+        width: 500,
+        height: 500,
         margin: {
             top: 10,
             bottom: 20,
@@ -9,12 +9,12 @@ d3.csv("dataaset/01-AB n_gender_year.csv").then(function(dataset){
             left: 50
         }
     }
-	var svg = d3.select("#linechart")
+	var svg = d3.select("#LineCt1a")
                 .style("width", dimensions.width)
                 .style("height", dimensions.height);
 	
 	var xAccessor=d=>d.Year
-	var yAccessor=d=>d.n_authors
+	var yAccessor=d=>+d.n_authors
 	
 	var xScale=d3.scaleLinear()
                   .domain(d3.extent(dataset,xAccessor))
@@ -24,17 +24,13 @@ d3.csv("dataaset/01-AB n_gender_year.csv").then(function(dataset){
                   .domain(d3.extent(dataset,yAccessor))
                   .range([dimensions.width - dimensions.margin.right,dimensions.margin.left])
 	
-	//const sumstat = d3.group(data, d => d.Group);
+	const male = dataset.filter(d => { return d.AuthorGender == 'Male' && d.Group=='All authors'});
+    const female = dataset.filter(d => {return d.AuthorGender == 'Female' && d.Group=='All authors'});
+    const unknown = dataset.filter(d => { return d.AuthorGender == 'Unknown' && d.Group=='All authors'});
 	
-//	const color = d3.scaleOrdinal()
-//					.range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
-	const male = dataset.filter(d => d.AuthorGender == 'Male');
-	const female = dataset.filter(d => d.AuthorGender == 'Female');
-	const unknown = dataset.filter(d => d.AuthorGender == 'Unknown');
-	
-	 var legend = svg.append('g')
-					 .attr('class', 'legend')
-                     .attr('transform', 'translate(' + (10) + ', 10)');
+//	 var legend = svg.append('g')
+//					 .attr('class', 'legend')
+//                     .attr('transform', 'translate(' + (10) + ', 10)');
 	
 	
 //	var color=			{if (d=>d.AuthorGender)return yellow
@@ -79,43 +75,6 @@ d3.csv("dataaset/01-AB n_gender_year.csv").then(function(dataset){
                   .style("fill","none" )
                   .style("stroke", "blue")
                   .style("stroke-width", "2");
-//	} else {
-		var MaleCurves=svg.append("path")
-				  .datum(male) 
-				  .attr("class", "line") 
-				  .attr("d", line)
-                  .style("fill","none" )
-                  .style("stroke", "yellow")
-                  .style("stroke-width", "2");
-				  
-	var FemaleCurve=svg.append("path")
-				  .datum(female) 
-				  .attr("class", "line") 
-				  .attr("d", line)
-                  .style("fill","none" )
-                  .style("stroke", "red")
-                  .style("stroke-width", "2");
-
-	var UnknownCurve=svg.append("path")
-				  .datum(unknown) 
-				  .attr("class", "line") 
-				  .attr("d", line)
-                  .style("fill","none" )
-                  .style("stroke", "blue")
-                  .style("stroke-width", "2");
-		
-/*	var lines=svg.selectAll("lines")
-				 .data(dataset)
-				 .enter()
-				 .append("path")
-				 .attr("d",d3.line()
-							 .x(d => xScale(xAccessor(d)))
-							 .y(d => yScale(yAccessor(d))))
-//                 .attr("x", d => xScale(xAccessor(d)))
-//                 .attr("y", d => yScale(yAccessor(d)))
-				 .attr("stroke","blue")
-				 .attr("stroke-width",1.0)
-*/				 
     var xAxisgen = d3.axisBottom().scale(xScale)
     var yAxisgen = d3.axisLeft().scale(yScale)
 	
