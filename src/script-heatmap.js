0d78032fbcac472ec300f472cbacc2e4727c06d6
Joel Kiran Kumar <joelkirankumar@Joels-MacBook-Pro.local>
2021-12-04 19:40:36 -0500

    refactoring

--- src/script-heatmap.js
+++ src/script-heatmap.js
@@ -48,7 +48,7 @@ svg.append("text")
 
 // Build color scale
 var myColor = d3.scaleLinear()
-  .range(["white", "#69b3a2"])
+  .range(["white", "maroon"])
   .domain([1, 100])
 
 //Read the data
@@ -95,7 +95,7 @@ d3.csv("dataset/6_coll_actual_count.csv", function (data) {
 
 
   // Add title to graph
-  
+
   // Add subtitle to graph
   svg.append("text")
     .attr("x", 90)
